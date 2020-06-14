e0bf2c10b6f37e9546457ff1d459dd81b82c6829
Ramji Rathore <=ramjirathore2802@gmail.com>
2020-06-14 18:07:12 +0530

    Updates

---
+++ precache-manifest.79ebde6fd7d8a1586b3219cc75ffc840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c58bc4462cbf4ba361b4ce41f90c7f3",
+    "url": "/ImageGallery/index.html"
+  },
+  {
+    "revision": "9ce1ab94d37236ac8471",
+    "url": "/ImageGallery/static/css/main.19f92c12.chunk.css"
+  },
+  {
+    "revision": "ae7d44ce9823a4677c3f",
+    "url": "/ImageGallery/static/js/2.7fc96625.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ImageGallery/static/js/2.7fc96625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce1ab94d37236ac8471",
+    "url": "/ImageGallery/static/js/main.fe8ae08f.chunk.js"
+  },
+  {
+    "revision": "04997688685ab66c382b",
+    "url": "/ImageGallery/static/js/runtime-main.222c88c7.js"
+  }
+]);
